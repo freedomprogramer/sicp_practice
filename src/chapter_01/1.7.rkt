2e9b395874317a59dff65b0187886de400ac956e
Lee <freedomprogramer@gmail.com>
2014-08-03 13:03:51 +0800

    add encapsulation for sqrt-iter

--- src/chapter_01/1.7.rkt
+++ src/chapter_01/1.7.rkt
@@ -1,3 +1,42 @@
 #lang racket
-(define (good-enough? guess x)
-  (< (abs (- (improve guess x) guess)) 0.00000001))
+(define (square x)
+  (* x x))
+
+(define (good_enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (new-if predicate then-clause else-clause)
+  (cond (predicate then-clause)
+        (else else-clause)))
+
+(define (sqrt-iter guess x)
+  (new-if (good_enough? guess x)
+      guess
+      (sqrt-iter (improve guess x) 
+                 x)))
+
+(sqrt-iter 2 4)
+(sqrt-iter 1 2)
+
+;;; 运行结果为‘Interactions disabled’
+;;; 因为lisp中是求参数值，所以在展开sqrt-iter中就无限展开了
+
+
+;;; 封装辅助过程
+(define (sqrt x)
+  (define (improve guess x)
+    (average guess (/ x guess)))
+  (define (good-enough? guess x)
+    (< (abs (- (improve guess x) guess)) 0.0000001))
+  (define (sqrt-iter guess x)
+    (if (good-enough? guess x)
+        guess
+        (sqrt-iter (improve guess x) x)))
+  (sqrt 1.0 x))
+  
