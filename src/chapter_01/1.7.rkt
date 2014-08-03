e7e8b693aa32889336830088e5fd7652ee3093e8
Lee <freedomprogramer@gmail.com>
2014-08-03 11:22:10 +0800

    rename 1.7 to 1.7 and finished 1.7

--- src/chapter_01/1.7.rkt
+++ src/chapter_01/1.7.rkt
@@ -1,28 +1,3 @@
 #lang racket
-(define (square x)
-  (* x x))
-
-(define (good_enough? guess x)
-  (< (abs (- (square guess) x)) 0.001))
-
-(define (average x y)
-  (/ (+ x y) 2))
-
-(define (improve guess x)
-  (average guess (/ x guess)))
-
-(define (new-if predicate then-clause else-clause)
-  (cond (predicate then-clause)
-        (else else-clause)))
-
-(define (sqrt-iter guess x)
-  (new-if (good_enough? guess x)
-      guess
-      (sqrt-iter (improve guess x) 
-                 x)))
-
-(sqrt-iter 2 4)
-(sqrt-iter 1 2)
-
-;;; 运行结果为‘Interactions disabled’
-;;; 因为lisp中是求参数值，所以在展开sqrt-iter中就无限展开了
+(define (good-enough? guess x)
+  (< (abs (- (improve guess x) guess)) 0.00000001))
